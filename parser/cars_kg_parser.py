0eb0fec7e104a27f7b809110e303a133f7b6550b
Veggyo <veggyscott@gmail.com>
2023-06-02 22:10:33 +0600

    сделала группого бота с баном и закрепом

--- parser/cars_kg_parser.py
+++ parser/cars_kg_parser.py
@@ -3,25 +3,26 @@ from bs4 import BeautifulSoup as BS
 
 URL = 'https://cars.kg/offers'
 page = 0
-
+end = 'По заданным критериям не найдено ни одного предложения'
 while True:
     page += 1
-    url = URL + f'{page}'
+    url = URL + f'/{page}'
     response = requests.get(url=URL, timeout=10)
+    # print(f"{response.status_code=}")
+    # print(f"{response.text[:270]=}")
     if response.status_code == 200:
-        end = 'По заданным критериям не найдено ни одного предложения'
-        for p in url:
-            if url == end:
-                break
-    soup = BS(response.text, 'html.parser')
-    cars_list = soup.find('div', class_='catalog-list')
+        # проверка что не вышли за последнюю стр
+        if response.text.find(end) > -1:
+            break
+        soup = BS(response.text, 'html.parser')
+        cars_list = soup.find('div', class_='catalog-list')
 
-    for c in cars_list.find_all('a', class_='catalog-list-item'):
-        model = c.find('span', class_='catalog-item-caption')
-        params = c.find('span', class_='catalog-item-params')
-        more = c.find('span', class_='catalog-item-descr')
-        info = c.find('span', class_='catalog-item-info')
-        views = c.find('span', class_='catalog-item-views')
-        year = model.find(class_='caption-year').text
-        model = model.text.replace(f" , {year}", '').strip()
-        print(model, params, more, info, views, year)
+        for c in cars_list.find_all('a', class_='catalog-list-item'):
+            model = c.find('span', class_='catalog-item-caption')
+            params = c.find('span', class_='catalog-item-params')
+            more = c.find('span', class_='catalog-item-descr')
+            info = c.find('span', class_='catalog-item-info')
+            views = c.find('span', class_='catalog-item-views')
+            year = model.find(class_='caption-year').text
+            model = model.text.replace(f" , {year}", '').strip()
+            print(model, params, more, info, views, year)
